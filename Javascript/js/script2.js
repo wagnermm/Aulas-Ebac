7e15019946d28874792ece16a89ca60a53ebb07d
Wagner Miranda <wagnermarquesdemiranda@gmail.com>
2022-06-09 16:19:56 -0300

    ajustando posicionamentos em elementos js, criado divs para cada input no html, validando as novas divs no js

--- Javascript/js/script2.js
+++ Javascript/js/script2.js
@@ -6,18 +6,129 @@
                 event.preventDefault();
         
                 if(this.value == ""){
-                    document.querySelector('.mensagem').innerHTML = "Por favor, verifique os campos destacados";
+                    document.querySelector('.mensagem-nome').innerHTML = "Por favor, informe o seu nome";
                     this.classList.add('erro');
                     this.parentNode.classList.add('erro');
                     return false;
                 } else {
-                    document.querySelector('.mensagem').innerHTML = "";
+                    document.querySelector('.mensagem-nome').innerHTML = "";
                     this.classList.remove('erro');
                     this.parentNode.classList.remove('erro');
                 }
         
-            });
+            }); 
+        }
+
+        let camposObrigatorios = document.querySelectorAll('input.obrigatorio');
+        for( let emFoco of camposObrigatorios) {
+            validaCampo(emFoco);
+        }
+
+        function validaCity(elemento){
+
+            elemento.addEventListener('focusout', function(event) {
         
+                event.preventDefault();
+        
+                if(this.value == ""){
+                    document.querySelector('.mensagem-city').innerHTML = "Por favor, informe sua cidade";
+                    this.classList.add('erro');
+                    this.parentNode.classList.add('erro');
+                    return false;
+                } else {
+                    document.querySelector('.mensagem-city').innerHTML = "";
+                    this.classList.remove('erro');
+                    this.parentNode.classList.remove('erro');
+                }
+        
+            }); 
+        }
+
+        let camposObrigatorios2 = document.querySelectorAll('input.obrigatorio2');
+        for( let emFoco of camposObrigatorios2) {
+            validaCity(emFoco);
+        }
+
+        
+        
+        function validaCep(elemento){
+        
+            elemento.addEventListener('focusout', function(event) {
+        
+                event.preventDefault();
+        
+                const cepValido = /^[0-9]{5}[-][0-9]{3}$/; 
+        
+                if(this.value.match(cepValido)){
+                    document.querySelector('.mensagem-cep').innerHTML = "";
+                    this.classList.remove('erro');
+                    this.parentNode.classList.remove('erro');
+                } else {
+                    document.querySelector('.mensagem-cep').innerHTML = "Por favor, informe seu cep";
+                    this.classList.add('erro');
+                    this.parentNode.classList.add('erro');
+                    return false;
+                }
+        
+            });
+        }
+
+        let camposCep = document.querySelectorAll('input.cep');
+        for( let emFoco of camposCep) {
+            validaCep(emFoco);
+        }
+        
+        
+        function validaEmail(elemento){
+        
+            elemento.addEventListener('focusout', function(event) {
+        
+                event.preventDefault();
+        
+                const emailValido = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?/i;
+                if(this.value.match(emailValido)) {
+                    document.querySelector('.mensagem-email').innerHTML = "";
+                    this.classList.remove('erro');
+                    this.parentNode.classList.remove('erro');
+                } else {
+                    document.querySelector('.mensagem-email').innerHTML = "Por favor, informe o seu e-mail";
+                    this.classList.add('erro');
+                    this.parentNode.classList.add('erro');
+                    return false;
+                }
+        
+            });
+        }
+
+        let camposEmail = document.querySelectorAll('input.email');
+        for( let emFoco of camposEmail) {
+            validaEmail(emFoco);
+        }
+
+
+        function validaCampoUf(elemento) {
+
+            elemento.addEventListener('focusout', function(event) {
+
+                event.preventDefault();
+
+                const ufValido = /^[a-z]{2}$/i;
+                if(this.value.match(ufValido)) {
+                    document.querySelector('.mensagem-uf').innerHTML = "";
+                    this.classList.remove('erro');
+                    this.parentNode.classList.remove('erro');
+                } else {
+                    document.querySelector('.mensagem-uf').innerHTML = "Por favor, informe sua UF";
+                    this.classList.add('erro');
+                    this.parentNode.classList.add('erro');
+                    return false;
+                }
+            });
+        }
+
+        let camposUf = document.querySelectorAll('input.uf');
+        for( let emFoco of camposUf) {
+            validaCampoUf(emFoco);
         }
 
         function disableButton() {
@@ -33,96 +144,4 @@
                 return
             }
             document.querySelector('#btn').disabled = true;
-        }
-        
-        function validaCep(elemento){
-        
-            elemento.addEventListener('focusout', function(event) {
-        
-                event.preventDefault();
-        
-                const cepValido = /^[0-9]{5}[-][0-9]{3}$/; 
-        
-                if(this.value.match(cepValido)){
-                    document.querySelector('.mensagem').innerHTML = "";
-                    this.classList.remove('erro');
-                    this.parentNode.classList.remove('erro');
-                } else {
-                    document.querySelector('.mensagem').innerHTML = "Por favor, verifique os campos destacados";
-                    this.classList.add('erro');
-                    this.parentNode.classList.add('erro');
-                    return false;
-                }
-        
-            });
-        
-        }
-        
-        
-        function validaEmail(elemento){
-        
-            elemento.addEventListener('focusout', function(event) {
-        
-                event.preventDefault();
-        
-                const emailValido = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?/i;
-                if(this.value.match(emailValido)) {
-                    document.querySelector('.mensagem').innerHTML = "";
-                    this.classList.remove('erro');
-                    this.parentNode.classList.remove('erro');
-                } else {
-                    document.querySelector('.mensagem').innerHTML = "Por favor, verifique os campos destacados";
-                    this.classList.add('erro');
-                    this.parentNode.classList.add('erro');
-                    return false;
-                }
-        
-            });
-        
-        }
-
-
-        function validaCampoUf(elemento) {
-
-            elemento.addEventListener('focusout', function(event) {
-
-                event.preventDefault();
-
-                const ufValido = /^[a-z]{2}$/i;
-                if(this.value.match(ufValido)) {
-                    document.querySelector('.mensagem').innerHTML = "";
-                    this.classList.remove('erro');
-                    this.parentNode.classList.remove('erro');
-                } else {
-                    document.querySelector('.mensagem').innerHTML = "Por favor, verifique os campos destacados";
-                    this.classList.add('erro');
-                    this.parentNode.classList.add('erro');
-                    return false;
-                }
-
-
-            });
-
-        }
-        
-        
-        let camposObrigatorios = document.querySelectorAll('input.obrigatorio');
-        let camposCep = document.querySelectorAll('input.cep');
-        let camposEmail = document.querySelectorAll('input.email');
-        let camposUf = document.querySelectorAll('input.uf');
-        
-        for( let emFoco of camposObrigatorios) {
-            validaCampo(emFoco);
-        }
-        
-        for( let emFoco of camposCep) {
-            validaCep(emFoco);
-        }
-        
-        for( let emFoco of camposEmail) {
-            validaEmail(emFoco);
-        }
-
-        for( let emFoco of camposUf) {
-            validaCampoUf(emFoco);
         }
