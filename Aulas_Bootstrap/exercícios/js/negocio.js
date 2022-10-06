8145d4ab3b89620a88990edf05cf76535cd8cf3b
Wagner Miranda <wagnermarquesdemiranda@gmail.com>
2022-10-06 14:20:51 -0300

    iniciando novo arquivo html/css/js em exercicios. introduzido novas imagens/libs em exercicios

---
+++ Aulas_Bootstrap/exercícios/js/negocio.js
@@ -0,0 +1,5 @@
+const myCarouselElement = document.querySelector('#carouselExampleFade')
+    const carousel = new bootstrap.Carousel(myCarouselElement, {
+        interval: 2000,
+            wrap: true
+        })
